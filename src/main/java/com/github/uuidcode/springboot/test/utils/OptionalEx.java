07b411604787221d5d80b0b419305d4cb4329560
uuidcode <uuidcode@gmail.com>
2017-08-18 17:24:36 +0900

    OptionalEx

--- src/main/java/com/github/uuidcode/springboot/test/utils/OptionalEx.java
+++ src/main/java/com/github/uuidcode/springboot/test/utils/OptionalEx.java
@@ -1,57 +1,83 @@
 package com.github.uuidcode.springboot.test.utils;
 
-import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class OptionalEx<T> {
-    private static final OptionalEx<?> EMPTY = new OptionalEx<>();
+    private Optional<T> optional;
 
-    private final T value;
-
-    public static <T> OptionalEx<T> ofNullable(T value) {
-        return value == null ? empty() : of(value);
+    private OptionalEx(Optional<T> optional) {
+        this.optional = optional;
     }
 
-    public static<T> OptionalEx<T> empty() {
-        @SuppressWarnings("unchecked")
-        OptionalEx<T> t = (OptionalEx<T>) EMPTY;
-        return t;
+    public static <T> OptionalEx<T> empty() {
+        return new OptionalEx<>(Optional.empty());
     }
 
     public static <T> OptionalEx<T> of(T value) {
-        return new OptionalEx<>(value);
+        return new OptionalEx<>(Optional.of(value));
     }
 
-    private OptionalEx(T value) {
-        this.value = Objects.requireNonNull(value);
+    public static <T> OptionalEx<T> ofNullable(T value) {
+        return new OptionalEx<>(Optional.ofNullable(value));
     }
 
-    private OptionalEx() {
-        this.value = null;
-    }
-
-    public <U> OptionalEx<T> mapAndIfPresent(Function<? super T, ? extends U> mapper, Consumer<? super U> consumer) {
-        this.map(mapper).ifPresent(consumer);
-        return this;
-    }
-
-    public<U> OptionalEx<U> map(Function<? super T, ? extends U> mapper) {
-        Objects.requireNonNull(mapper);
-        if (!isPresent())
-            return empty();
-        else {
-            return OptionalEx.ofNullable(mapper.apply(value));
-        }
+    public T get() {
+        return optional.get();
     }
 
     public boolean isPresent() {
-        return value != null;
+        return optional.isPresent();
     }
 
     public void ifPresent(Consumer<? super T> consumer) {
-        if (value != null) {
-            consumer.accept(value);
-        }
+        optional.ifPresent(consumer);
+    }
+
+    public OptionalEx<T> filter(Predicate<? super T> predicate) {
+        return new OptionalEx<>(optional.filter(predicate));
+    }
+
+    public <U> OptionalEx<U> map(Function<? super T, ? extends U> mapper) {
+        return new OptionalEx<>(optional.map(mapper));
+    }
+
+    public <U> OptionalEx<T> mapAndIfPresent(Function<? super T, ? extends U> mapper, Consumer<? super U> consumer) {
+        optional.map(mapper).ifPresent(consumer);
+        return this;
+    }
+
+    public <U> OptionalEx<U> flatMap(Function<? super T, Optional<U>> mapper) {
+        return new OptionalEx<>(optional.flatMap(mapper));
+    }
+
+    public T orElse(T other) {
+        return optional.orElse(other);
+    }
+
+    public T orElseGet(Supplier<? extends T> other) {
+        return optional.orElseGet(other);
+    }
+
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
+        return optional.orElseThrow(exceptionSupplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return optional.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return optional.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return optional.toString();
     }
 }
