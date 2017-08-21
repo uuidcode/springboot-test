c690658914e1104aa67be24cd7fe2f3b0ba5cc8b
uuidcode <uuidcode@gmail.com>
2017-08-21 13:13:14 +0900

    PageImplTest

--- src/test/java/org/springframework/data/domain/PageImplTest.java
+++ src/test/java/org/springframework/data/domain/PageImplTest.java
@@ -1,12 +1,14 @@
 package org.springframework.data.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.data.domain.Sort.Direction.ASC;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
+import org.springframework.data.domain.Sort.Order;
 
 public class PageImplTest {
     private List<Integer> totalList = IntStream.range(0, 125)
@@ -28,16 +30,27 @@ public class PageImplTest {
 
         assertThat(page.getTotalPages()).isEqualTo(13);
         assertThat(page.getTotalElements()).isEqualTo(125);
+        assertThat(page.getNumberOfElements()).isEqualTo(10);
         assertThat(page.hasNext()).isTrue();
+        assertThat(page.hasPrevious()).isFalse();
+        assertThat(page.isFirst()).isTrue();
         assertThat(page.isLast()).isFalse();
+        assertThat(pageable.hasPrevious()).isFalse();
     }
 
     @Test
     public void last() {
-        Pageable pageable = new PageRequest(12, 10);
+        Sort sort = new Sort(new Order(ASC, "projectId"));
+        Pageable pageable = new PageRequest(12, 10, sort);
         PageImpl page = new PageImpl(this.getList(pageable), pageable, totalList.size());
 
         assertThat(page.hasNext()).isFalse();
+        assertThat(page.hasPrevious()).isTrue();
         assertThat(page.isLast()).isTrue();
+        assertThat(page.isFirst()).isFalse();
+        assertThat(page.getTotalElements()).isEqualTo(125);
+        assertThat(page.getNumberOfElements()).isEqualTo(5);
+        assertThat(pageable.getOffset()).isEqualTo(120);
+        assertThat(pageable.hasPrevious()).isTrue();
     }
 }
