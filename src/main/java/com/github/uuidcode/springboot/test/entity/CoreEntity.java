3d680902c0101bb21c571aa90afaa394ef130759
uuidcode <uuidcode@gmail.com>
2017-08-21 16:49:52 +0900

    orderBy

--- src/main/java/com/github/uuidcode/springboot/test/entity/CoreEntity.java
+++ src/main/java/com/github/uuidcode/springboot/test/entity/CoreEntity.java
@@ -4,39 +4,52 @@ import java.io.Serializable;
 
 import javax.persistence.Transient;
 
+import org.springframework.data.domain.PageRequest;
+
 public class CoreEntity<T> implements Serializable {
     @Transient
-    private Long page;
+    private Integer page;
     @Transient
-    private Long size;
+    private Integer size;
+    @Transient
+    private String orderBy;
+    
+    public String getOrderBy() {
+        return this.orderBy;
+    }
+    
+    public CoreEntity setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+        return this;
+    }
 
-    public Long getSize() {
-        if (this.size == null) {
-            this.size = 10L;
-        }
-
+    public Integer getSize() {
         return this.size;
     }
 
-    public T setSize(Long size) {
+    public T setSize(Integer size) {
         this.size = size;
         return (T) this;
     }
 
-    public Long getPage() {
-        if (this.page == null) {
-            this.page = 1L;
-        }
-
+    public Integer getPage() {
         return this.page;
     }
 
-    public T setPage(Long page) {
+    public T setPage(Integer page) {
         this.page = page;
         return (T) this;
     }
 
-    public Long getOffset() {
-        return (this.page - 1L) * this.size;
+    public PageRequest getPageRequest() {
+        if (this.size == null) {
+            this.size = 10;
+        }
+
+        if (this.page == null) {
+            this.page = 1;
+        }
+
+        return new PageRequest(this.page - 1, this.size);
     }
 }
