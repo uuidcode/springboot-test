e866b8515f7b7b4b4fd294b31bb032730853b1ed
uuidcode <uuidcode@gmail.com>
2017-08-21 10:32:38 +0900

    JPAExpressions

--- src/main/java/com/github/uuidcode/springboot/test/service/ProjectService.java
+++ src/main/java/com/github/uuidcode/springboot/test/service/ProjectService.java
@@ -13,11 +13,13 @@ import com.github.uuidcode.springboot.test.entity.Author;
 import com.github.uuidcode.springboot.test.entity.Partner;
 import com.github.uuidcode.springboot.test.entity.Project;
 import com.github.uuidcode.springboot.test.entity.QAuthor;
+import com.github.uuidcode.springboot.test.entity.QEpisode;
 import com.github.uuidcode.springboot.test.entity.QPartner;
 import com.github.uuidcode.springboot.test.entity.QProject;
 import com.github.uuidcode.springboot.test.entity.QProjectAuthorMap;
 import com.github.uuidcode.springboot.test.utils.CoreUtil;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 
 @Service
@@ -27,6 +29,7 @@ public class ProjectService extends CoreService<Project> {
     private QProjectAuthorMap qProjectAuthorMap = QProjectAuthorMap.projectAuthorMap;
     private QAuthor qAuthor = QAuthor.author;
     private QPartner qPartner = QPartner.partner;
+    private QEpisode qEpisode = QEpisode.episode;
 
     @Resource
     private PartnerService partnerService;
@@ -74,14 +77,17 @@ public class ProjectService extends CoreService<Project> {
     }
 
     private JPAQuery<Tuple> selectFromWhere() {
-        return this.select(qProject, qPartner, qAuthor)
+        return this.select(qProject, qPartner, qAuthor,
+                JPAExpressions.select(qEpisode.count())
+                .from(qEpisode)
+                .where(qEpisode.projectId.eq(qProject.projectId)))
             .from(qProject)
             .leftJoin(qPartner)
                 .on(qPartner.projectId.eq(qProject.projectId))
             .leftJoin(qProjectAuthorMap)
                 .on(qProjectAuthorMap.projectId.eq(qProject.projectId))
             .leftJoin(qAuthor)
-                .on(qAuthor.authorId.eq(qAuthor.authorId))
+                .on(qAuthor.authorId.eq(qProjectAuthorMap.authorId))
             .where(qProject.projectType.eq(Project.ProjectType.NORMAL));
     }
 
